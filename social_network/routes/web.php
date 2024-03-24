f2c897e492e2c30b7e5b208536e0037085160daa
Nthanhnhan265 <ngthanhnhan265.xb@gmail.com>
2024-03-24 09:12:18 +0700

    main update

---
+++ social_network/routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('index', function () {
+//     return view('index');
+// });
+// Route::get('logout', function () {
+//     return view('logout');
+// });
+
+// Route::get('/{namePage?}', function ($namePage = "index") {
+//     $pages = [
+//         'login' => 'login',
+//         'logout' => 'logout',
+//         'messages' => 'messages',   
+//         'index-2' => 'index-2',
+//         'index2' => 'index2',
+//         'index-company' => 'index-company',
+//         'inbox' => 'inbox',
+//         'insight' => 'insight',
+//         'insights' => 'insights',
+//         'knowledge-base' => 'knowledge-base',
+//         'landing' => 'landing',
+//         'location' => 'location',
+//         'newsfeed' => 'newsfeed',
+//         'notifications' => 'notifications',
+//         'page-likers' => 'page-likers',
+//         'page' => 'page',
+//         'widgets' => 'widgets'
+//     ];
+
+//     return isset($pages[$namePage]) ? view($pages[$namePage]) : view('error');
+// });
+Route::get('/{page?}', function ($page = "index") {  
+    return view($page);
+});

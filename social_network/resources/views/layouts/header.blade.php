25d1c02a79d2a27a61124ec8329a19117271ebb6
Nthanhnhan265 <ngthanhnhan265.xb@gmail.com>
2024-04-01 12:10:16 +0700

    some changes about extends

--- social_network/resources/views/layouts/header.blade.php
+++ social_network/resources/views/layouts/header.blade.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-	<link rel="icon" href="images/fav.png" type="image/png" sizes="16x16"> 
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+	<title>Winku Social Network Toolkit</title>
+	<link rel="icon" href="{{ asset('images/fav.png') }}" type="image/png" sizes="16x16"> 
     
-	<link rel="stylesheet" href="{{asset('css/main.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/main.min.css')}}">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link rel="stylesheet" href="{{asset('css/color.css')}}">
     <link rel="stylesheet" href="{{asset('css/responsive.css')}}">
+
 </head>
 <body>
 <div class="theme-layout">
@@ -202,7 +205,3 @@
 		</div>
 	</nav>
 </div>
-	<script src="{{asset('js/script.js')}}"></script>
-	<script src="{{asset('js/main.min.js')}}"></script>
-</body>
-</html>
